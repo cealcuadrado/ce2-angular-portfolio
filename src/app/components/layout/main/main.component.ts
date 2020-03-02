1acb123392f2fe373f3d04dce9713abfa97dba04
cristiancajiaos <cristian.cajiao@alumnos.usm.cl>
2020-03-02 18:00:03 -0300

    Definida forma en como se muestran los blogs en el main

--- src/app/components/layout/main/main.component.ts
+++ src/app/components/layout/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit} from '@angular/core';
+import { BlogsService } from 'src/app/services/blogs.service';
+import { Blog } from 'src/app/interfaces/blog';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-main',
@@ -6,9 +9,32 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  constructor() { }
+  blogs: Blog[];
+  tiles: any[];
+  cols: number;
+  rowHeight: string;
+  showBreak: boolean;
+
+  constructor(
+    private blogsService: BlogsService,
+    private breakpointObserver: BreakpointObserver
+  ) { }
 
   ngOnInit() {
-  }
+    this.blogs = this.blogsService.getBlogs();
 
+    this.tiles = [];
+
+    this.blogs.forEach(e => {
+      this.tiles.push({ cols: 1, rows: 1, blog: e });
+    });
+
+    const layoutChanges = this.breakpointObserver.observe(['(max-width:991px)']);
+
+    layoutChanges.subscribe(result => {
+      this.cols = (result.matches) ? 1 : 3;
+      this.rowHeight = (result.matches) ? '150px' : '300px';
+      this.showBreak = !result.matches;
+    });
+  }
 }
